4cf9d6f66efcf94ad6a9233dd36eff684e9a3c38
Ginny Yadav <ginny@tidepool.org>
2020-09-19 23:35:53 -0400

    modals and tooltips

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: [
+    'html',
+  ],
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
